64768217b4218fb1db0b77c3039b2f5f7f4535e3
u8p <154946576+xap8Em@users.noreply.github.com>
2024-08-01 11:28:56 +0900

    Make window mode toggleable

--- main/main.gd
+++ main/main.gd
@@ -1,1 +1,18 @@
 extends Node
+
+
+var _window_mode_is_fullscreen: bool
+
+
+func _init() -> void:
+	_window_mode_is_fullscreen = bool(DisplayServer.window_get_mode())
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("toggle_window_mode"):
+		_toggle_window_mode()
+
+
+func _toggle_window_mode() -> void:
+	_window_mode_is_fullscreen = not _window_mode_is_fullscreen
+	DisplayServer.window_set_mode(3 * int(_window_mode_is_fullscreen))
